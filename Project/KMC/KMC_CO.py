9a4eaee9635f9ffd42c598b715f348a5928c71b9
lot nikee <lotnikee@github.com>
2025-01-09 21:10:14 +0000

    commit

--- Project/KMC/KMC_CO.py
+++ Project/KMC/KMC_CO.py
@@ -1,34 +1,54 @@
 import pandas as pd 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import numpy as np 
 
-# Reading the Excel sheet with all the adsorption isotherms
 data = pd.ExcelFile('~/Desktop/TCC_SM_/Project/KMC/KMC_CO.xlsx')
 KMC_CO_data_sheet = data.parse('Sheet1') 
 pressure_column = KMC_CO_data_sheet.columns[0]
 coverage_column = KMC_CO_data_sheet.columns[3]
 
-filtered_KMC_CO_data = KMC_CO_data_sheet[[pressure_column, coverage_column]].iloc[15:27]
+# Selecting rows 6-14 (inclusive)
+filtered_KMC_CO_data = KMC_CO_data_sheet.iloc[4:15]  # Python uses zero-based indexing
+filtered_KMC_CO_data = filtered_KMC_CO_data[[pressure_column, coverage_column]]
+
+# Rename columns
 filtered_KMC_CO_data.columns = ['p', 'Coverage per site']
 
-# Plotting
+# Convert columns to numeric
+filtered_KMC_CO_data['p'] = pd.to_numeric(filtered_KMC_CO_data['p'], errors='coerce')
+filtered_KMC_CO_data['Coverage per site'] = pd.to_numeric(filtered_KMC_CO_data['Coverage per site'], errors='coerce')
+
+# Drop rows with NaN values
+filtered_KMC_CO_data = filtered_KMC_CO_data.dropna()
+
+delta_g = 0.106  # eV 
+T = 300  # K
+boltzmann = 8.6173e-05  # eV / K
+P = np.logspace(-2, 6, num =500)
+
+constant = -delta_g / (T * boltzmann)
+K = np.exp(constant)
+theta = (K * P) / (1 + (K * P))
+
 plt.figure(figsize=(10, 8))
-plt.plot(filtered_KMC_CO_data['p'], filtered_KMC_CO_data['Coverage per site'], label='KMC Data', color='red', marker="o", linestyle="")
-plt.xlabel('Pressure (Pa)', fontsize=14, weight='bold')
+plt.plot(P, theta, label='Langmuir isotherm', color='red')
+plt.plot(filtered_KMC_CO_data['p'], filtered_KMC_CO_data['Coverage per site'], label='KMC Data', color='red', marker='o', linestyle="")
+
+plt.xlabel('Pressure (bar)', fontsize=14, weight='bold')
 plt.ylabel('Surface coverage', fontsize=14, weight='bold')
 plt.title('Surface coverage vs pressure', fontsize=16, weight='bold')
 
 # Custom x-axis scale and ticks
 plt.xscale("log")
-plt.xlim(0.5e3, 1.5e11)  # Set x-axis limits
 
 # Manually specify major ticks
-major_ticks = [1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9, 1e10, 1e11]
+major_ticks = [1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6]
 plt.xticks(major_ticks, [f"$10^{{{int(np.log10(tick))}}}$" for tick in major_ticks])  # Custom tick labels
 
 # Grid and layout
 plt.grid(True, which="both", linestyle="--", linewidth=0.5)  # Grid for both major and minor ticks
 plt.legend(loc='upper left', fontsize=8)
-plt.ylim(-0.05, 1.05)
 plt.tight_layout()
 plt.show()
+
+
